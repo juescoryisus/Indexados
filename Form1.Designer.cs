271044083269d80d1c65c77dea3ed7d5100eee66
juescoryisus <jesuseduardog204@gmail.com>
2024-03-09 23:03:26 -0600

    primer repositorio

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Secuencialesindexados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(77, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(53, 41);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(151, 26);
+            this.txtDato.TabIndex = 3;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(53, 163);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(151, 26);
+            this.txtBuscar.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 271);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.TextBox txtBuscar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Secuencialesindexados
+{
+    public partial class Form1 : Form
+    {
+        private const string indiceFileName = "index.txt";
+        private const string datosFileName = "datos.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamWriter swDatos = new StreamWriter(datosFileName, true))
+                using (StreamWriter swIndice = new StreamWriter(indiceFileName, true))
+                {
+                    long posicion = swDatos.BaseStream.Length; // Obtiene la posición del próximo registro en el archivo de datos
+
+                    string dato = txtDato.Text;
+                    swDatos.WriteLine(dato);
+
+                    string indice = $"{posicion},{dato}"; // Formato del índice: posición,dato
+                    swIndice.WriteLine(indice);
+                }
+
+                MessageBox.Show("Dato agregado correctamente.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtDato.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar el dato: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string datoABuscar = txtBuscar.Text;
+
+                using (StreamReader sr = new StreamReader(indiceFileName))
+                {
+                    string linea;
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        string[] partes = linea.Split(',');
+                        if (partes.Length == 2 && partes[1] == datoABuscar)
+                        {
+                            long posicion = long.Parse(partes[0]);
+                            using (StreamReader srDatos = new StreamReader(datosFileName))
+                            {
+                                srDatos.BaseStream.Seek(posicion, SeekOrigin.Begin);
+                                string datoEncontrado = srDatos.ReadLine();
+                                MessageBox.Show($"El dato '{datoABuscar}' se encontró en la posición {posicion}: {datoEncontrado}", "Resultado de la búsqueda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show($"El dato '{datoABuscar}' no se encontró.", "Resultado de la búsqueda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al buscar el dato: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
